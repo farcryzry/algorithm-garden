74b6cf5f4013e259e15068894cc1c465b8c66f3a
Ruiyun Zhou <farcryzry@gmail.com>
2014-10-23 12:44:41 -0700

    ValidateBST2

--- src/ValidateBST.java
+++ src/ValidateBST.java
@@ -28,6 +28,18 @@ public class ValidateBST {
         return isLeftValid && isValidBST1(root.right);
     }
 
+    public static boolean isValidBST2(TreeNode root) {
+        return helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean helper(TreeNode root, int min, int max) {
+        if(root == null) return true;
+
+        if(root.val <= min || root.val >= max) return false;
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+
     public static void main(String[] args) {
 //            1
 //           / \
